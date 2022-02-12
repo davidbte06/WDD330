344a4f3837ee481e6044f425bc85dd0c29998622
davidbte06 <davidbte06@gmail.com>
2022-02-12 11:05:22 -0600

    fixed errors

--- todo-app/js/main.js
+++ todo-app/js/main.js
@@ -15,12 +15,15 @@ filterOption.addEventListener("click", filterTodo);
 function addTodo(e) {
     //Prevent natural behaviour
     e.preventDefault();
+
     //Create todo div
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
+
     //Create list
     const newTodo = document.createElement("li");
     newTodo.innerText = todoInput.value;
+
     //Save to local - do this last
     //Save to local
     saveLocalTodos(todoInput.value);
@@ -28,16 +31,19 @@ function addTodo(e) {
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
     todoInput.value = "";
+
     //Create Completed Button
     const completedButton = document.createElement("button");
     completedButton.innerHTML = `<i class="fas fa-check"></i>`;
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
+
     //Create trash button
     const trashButton = document.createElement("button");
     trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
     trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
+
     //attach final Todo
     todoList.appendChild(todoDiv);
 }
