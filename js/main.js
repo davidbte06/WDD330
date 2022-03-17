cc37121934340d21795f1e851da58b6185958f32
davidbte06 <davidbte06@gmail.com>
2022-03-17 00:43:53 -0600

    added weeks 9,10

--- js/main.js
+++ js/main.js
@@ -29,6 +29,14 @@ const array = [{
     {
         label: 'Week 8 Notes',
         link: 'week8/index.html'
+    },
+    {
+        label: 'Week 9 Notes',
+        link: 'week9/index.html'
+    },
+    {
+        label: 'Week 10 Notes',
+        link: 'week10/index.html'
     }
 
 ];
